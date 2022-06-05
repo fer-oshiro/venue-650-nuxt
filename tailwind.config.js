183ecd821d2d790dcb1077cf73e04436bc823d8d
fer-oshiro <fernanda.sayuri@outlook.com>
2022-06-05 17:03:03 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ['Great Vibes'],
+        title: ['Cormorant'],
+        body: ['Montserrat'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
