e65f724ed4848d6857dda02f904c81d7dc8e30fd
fer-oshiro <fernanda.sayuri@outlook.com>
2022-06-05 21:04:48 -0300

    add header responsivo

--- components/Link.vue
+++ components/Link.vue
@@ -1,5 +1,7 @@
 <template>
-  <li class="border-b-2 border-secondary1 hover:border-secondary2 py-2">
+  <li
+    class="py-2 px-4 bg-secondary2 border-b-2 border-secondary1 hover:bg-secondary3 hover:text-secondary2 md:bg-secondary1 md:px-0 md:hover:border-secondary2 md:hover:bg-secondary1 md:hover:text-secondary3"
+  >
     <NuxtLink :to="{ name: link }"><slot /></NuxtLink>
   </li>
 </template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex justify-between items-center p-5 border-2 border-secondary2">
-    <NuxtLink class="font-logo text-5xl" to="/">Venue 650</NuxtLink>
+  <div
+    class="flex flex-col justify-between items-center p-5 border-2 border-secondary2 md:flex-row"
+  >
+    <NuxtLink class="font-logo text-4xl md:text-5xl" to="/">Venue 650</NuxtLink>
 
-    <nav>
-      <ul class="font-body flex space-x-10">
+    <nav class="pt-10 md:pt-0">
+      <ul class="font-body flex flex-wrap justify-center gap-x-10 gap-y-5">
         <Link link="">Sobre</Link>
         <Link link="/projeto">Projeto</Link>
         <Link link="">Orçamento</Link>
