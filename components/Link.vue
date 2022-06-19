0026e848f302eca194470fa94dffbfdd10f465cd
fer-oshiro <fernanda.sayuri@outlook.com>
2022-06-19 13:45:01 -0300

    Fix header

--- components/Link.vue
+++ components/Link.vue
@@ -1,6 +1,6 @@
 <template>
   <li
-    class="py-2 px-4 bg-secondary2 border-b-2 border-secondary1 hover:bg-secondary3 hover:text-secondary2 md:bg-secondary1 md:px-0 md:hover:border-secondary2 md:hover:bg-secondary1 md:hover:text-secondary3"
+    class="py-2 px-4 bg-secondary2-100 border-b-2 border-secondary1 hover:bg-secondary3 hover:text-secondary1 md:bg-secondary1 md:px-0 md:hover:border-secondary2-100 md:hover:bg-secondary1 md:hover:text-secondary3"
   >
     <NuxtLink :to="{ path: '/', hash: link }"><slot /></NuxtLink>
   </li>
